9c3f5f35245a08cb48dfe4b0558c1d5d0b39aaba
Volkova-Natalia <volkova.natalia.1988@gmail.com>
2021-02-20 21:00:06 +0300

    (m) change User model - closer to embedded django User.

--- django_rest_auth_email_confirm_reset/models/user/user.py
+++ django_rest_auth_email_confirm_reset/models/user/user.py
@@ -1,17 +1,18 @@
 from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
 from django.db import models
+from django.utils import timezone
 
 from .managers import UserManager
 
 
 class User(AbstractBaseUser, PermissionsMixin):
-    email = models.EmailField(max_length=254, unique=True)
+    email = models.EmailField(max_length=254, null=False, blank=False, unique=True)
     name = models.CharField(max_length=254, null=True, blank=True)
     is_staff = models.BooleanField(default=False)
     is_active = models.BooleanField(default=True)
     is_superuser = models.BooleanField(default=False)
     last_login = models.DateTimeField(null=True, blank=True)
-    date_joined = models.DateTimeField(auto_now_add=True)
+    date_joined = models.DateTimeField(default=timezone.now)
 
     USERNAME_FIELD = 'email'
     EMAIL_FIELD = 'email'

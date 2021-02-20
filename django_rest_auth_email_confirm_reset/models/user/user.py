d5f12d8f632220046d1a398650e9bc7ca2e97dff
Volkova-Natalia <volkova.natalia.1988@gmail.com>
2021-02-20 16:55:05 +0300

    (m) revert __str__()

--- django_rest_auth_email_confirm_reset/models/user/user.py
+++ django_rest_auth_email_confirm_reset/models/user/user.py
@@ -24,7 +24,7 @@ class User(AbstractBaseUser, PermissionsMixin):
     # --------------------------------------------------
 
     def __str__(self):
-        return str(self.email)
+        return self.email
 
     def get_absolute_url(self):
         return "/users/%i/" % (self.pk)
